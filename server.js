24e193780e4622da9af7d97d535ec3b2bf84d4fa
Srinjay <srinjay@Srinjays-MacBook-Pro.local>
2025-10-04 02:11:36 -0700

    port 8080

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public')) //serve our files in public statically
-app.listen(5000)
+app.listen(8080, () => console.log('Server started on http://localhost:8080'))
