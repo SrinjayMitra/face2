f5bd2b33db9066cbf8d4b18f3930bd3b352df73b
SrinjayMitra <srinjaymitra918@gmail.com>
2025-10-18 00:09:10 -0700

    3000 port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public')) //serve our files in public statically
-app.listen(8080, () => console.log('Server started on http://localhost:8080'))
+app.listen(3000, () => console.log('Server started on http://localhost:3000'))
