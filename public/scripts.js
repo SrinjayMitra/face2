9fe301c042c9a9361514d9ab2c6c27a361808c61
SrinjayMitra <srinjaymitra918@gmail.com>
2026-01-16 13:34:22 -0800

    move closer phoen bug fix test

--- public/scripts.js
+++ public/scripts.js
@@ -154,7 +154,7 @@ const run = async () => {
       console.log("Face ratio:", faceRatio.toFixed(2));
 
       /* ===== DISTANCE CHECK ===== */
-      if (faceRatio < 0.25) {
+      if (faceRatio < 0.45) {
         showCenterMessage(["Move closer to your phone"], 3000);
         return;
       }
@@ -204,7 +204,7 @@ const run = async () => {
       if (currentTargetIndex === 0 && neutralVertical === null) {
         // if ( neutralVertical === null) {
         // Distance check
-        if (faceRatio > 0.3) {
+        if (faceRatio < 0.45) {
           showCenterMessage(["Move closer to your phone"], 500);
           direction = null;
           return;
@@ -299,7 +299,7 @@ const run = async () => {
         return;
       } else if (currentTargetIndex === 0) {
         // ❌ Too far
-        if (faceRatio < 0.25) {
+        if (faceRatio < 0.45) {
           showCenterMessage(["Move closer to your phone"], 500);
           direction = null; // block capture
         }
