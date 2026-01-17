94ac2c59963e4a1717dffccd2cd90a3b61c01fb6
SrinjayMitra <srinjaymitra918@gmail.com>
2026-01-16 17:53:21 -0800

    fixed up doew bug fix

--- public/scripts.js
+++ public/scripts.js
@@ -193,7 +193,7 @@ const run = async () => {
       const NEUTRAL_VERTICAL = 51.97; // straight-looking baseline
       const PRE_VERTICAL_CENTER = 52; // approx straight (your observed value)
       const PRE_VERTICAL_TOLERANCE = 0; // allow small error before calibration
-      const FINAL_VERTICAL_TOLERANCE = 13;
+      const FINAL_VERTICAL_TOLERANCE = 10;
       const VERTICAL_SAMPLE_COUNT = 12;
 
       let direction = "Center";
